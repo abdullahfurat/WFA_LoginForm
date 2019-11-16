d888acacfe6bdd5dda86900cfd6b0a2980282a47
abdullahfurat <a_furat@hotmail.com>
2019-11-16 21:00:51 +0300

    ödev tamam

--- WFA_UserLogin/Form1.cs
+++ WFA_UserLogin/Form1.cs
@@ -13,9 +13,62 @@ namespace WFA_UserLogin
 {
     public partial class Form1 : MetroForm
     {
+        Random rnd = new Random();
+        int kullanilanhak = 5;
         public Form1()
         {
             InitializeComponent();
+            lblRastgeleSayi.Text = rnd.Next(10000000, 100000000).ToString();
+            int kullanilanhak = 5;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(txtGirisAlani.Text== lblRastgeleSayi.Text)
+            {
+                MessageBox.Show("Giriş başarılıdır.");
+            }
+
+            else 
+            {
+                lblRastgeleSayi.Text = rnd.Next(10000000, 100000000).ToString();
+                txtGirisAlani.Text = "";
+                txtGirisAlani.Focus();
+                switch (kullanilanhak)
+                {
+                    case 5:
+                        star1.Visible = false;
+                    break;
+                    case 4:
+                        star2.Visible = false;
+                        break;
+                    case 3:
+                        star3.Visible = false;
+                        break;
+                    case 2:
+                        star4.Visible = false;
+                        break;
+                    case 1:
+                        star5.Visible = false;
+                        break;
+                }
+                kullanilanhak -=1;
+                if (kullanilanhak == 0)
+                {
+                    txtGirisAlani.Enabled = false;
+                    btnLogin.Enabled = false;
+                    lblRastgeleSayi.Text = "* * * * * * * *";
+                    MessageBox.Show("Giriş sağlanamadı");
+                    star1.Visible = true;
+                    star2.Visible = true;
+                    star3.Visible = true;
+                    star4.Visible = true;
+                    star5.Visible = true;
+                }
+
+            }
+
+
         }
     }
 }
